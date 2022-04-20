091e9aec0d2d5f3f3b6225828f80a0f833e22f7e
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-20 13:13:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { marked } from 'marked';
 import './App.css';
 
+marked.setOptions({
+  breaks: true
+})
+
+const renderer = new marked.Renderer();
+
 function App() {
+  const [text, setText] = React.useState('');
+
+  function handleText(e) {
+    setText(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quote-box">
+      <div id="preview">I'm a preview</div>
+      <textarea id="editor" name="text" value={text} onChange={(e) => handleText(e)}>I'm a textarea</textarea>
+      <h2>Output:</h2>
+      <Preview markdown={text} />
     </div>
   );
+
+  function Preview({markdown}){
+    return (
+      <div 
+        dangerouslySetInnerHTML={{
+          __html: marked(markdown, {renderer: renderer}),
+        }}
+        id="prev"
+      />
+    )
+  }
 }
 
 export default App;
