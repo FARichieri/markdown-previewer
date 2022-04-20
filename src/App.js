8d882131f94c9d6a0ef1e86dbc8e83cee92a5989
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-20 15:22:24 -0300

    OK

--- src/App.js
+++ src/App.js
@@ -1,39 +1,56 @@
 import React, { useState } from 'react';
 import { marked } from 'marked';
 import './App.css';
-
-marked.setOptions({
-  breaks: true
-})
-
-const renderer = new marked.Renderer();
+import Prism from 'prismjs';
 
 function App() {
-  const [text, setText] = React.useState('');
+  const [content, setContent] = React.useState('');
 
   function handleText(e) {
-    setText(e.target.value)
+    setContent(e.target.value)
   }
 
   return (
-    <div className="quote-box">
-      <div id="preview">I'm a preview</div>
-      <textarea id="editor" name="text" value={text} onChange={(e) => handleText(e)}>I'm a textarea</textarea>
-      <h2>Output:</h2>
-      <Preview markdown={text} />
-    </div>
+    <>
+      <Editor content={content} handleText={handleText}/>
+      <Preview markdown={content} />
+    </>
   );
+}
 
-  function Preview({markdown}){
+  function Editor({ content, handleText }) {
+    return(
+      <div className="quote-box">
+        <h2>Editor:</h2>
+        <textarea id="editor" type="text" name="content" value={content} onChange={handleText}>I'm a textarea</textarea>
+        <h2>Output:</h2>
+      </div>
+    )
+  }
+
+  function Preview({markdown}) {
+
+    marked.setOptions({
+      breaks: true,
+      highlight: function (code) {
+        return Prism.highlight(code, Prism.languages.javascript, "javascript");
+      }
+    });
+    
+    const renderer = new marked.Renderer();
+    renderer.link = function (href, title, text) {
+      return `<a target="_blank" href="${href}">${text}</a>`;
+    };
+    
     return (
       <div 
+        id="preview"
         dangerouslySetInnerHTML={{
-          __html: marked(markdown, {renderer: renderer}),
+          __html: marked(markdown, { renderer: renderer })
         }}
-        id="prev"
       />
     )
   }
-}
+
 
 export default App;
