8ae3ae4879f7716512a38cf43eb4240268117186
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-20 23:43:37 -0300

    OK

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { marked } from 'marked';
 import './App.css';
 import Prism from 'prismjs';
 
 function App() {
-  const [content, setContent] = React.useState('');
+  const [content, setContent] = React.useState(placeholder);
 
   function handleText(e) {
     setContent(e.target.value)
@@ -12,18 +12,22 @@ function App() {
 
   return (
     <>
-      <Editor content={content} handleText={handleText}/>
+      <Editor content={content} handleText={handleText} setContent={setContent}/>
       <Preview markdown={content} />
     </>
   );
 }
 
-  function Editor({ content, handleText }) {
+  function Editor({ content, handleText, setContent }) {
+    function handleClick() {
+      setContent('');
+    }
     return(
       <div className="quote-box">
-        <h2>Editor:</h2>
+        <h2 className='toolbar'>Editor:</h2>
         <textarea id="editor" type="text" name="content" value={content} onChange={handleText}>I'm a textarea</textarea>
-        <h2>Output:</h2>
+        <button className='cleaner' onClick={handleClick}>Clean Editor</button>
+        <h2 className='toolbar'>Previewer:</h2>
       </div>
     )
   }
@@ -51,6 +55,53 @@ function App() {
       />
     )
   }
+
+const placeholder = 
+`# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.linkedin.com/in/fabricio-richieri-383646217/), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+    - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![frichieri-developer-img](https://avatars.githubusercontent.com/u/85033184?v=4)
+> By [frichieri](https://github.com/FARichieri)
+`;
 
 
 export default App;
